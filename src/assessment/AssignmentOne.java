b39b8db06aaddbfc7c41d378761d84c13fca7161
ATaylorSu <963560148@qq.com>
2025-11-06 14:48:18 +0800

    Feature: Complete the writing of the main class Assign One, which is used to concatenate the functions of Part 3 (ClassObject Demo) and Part 5 (Appointment Manager), and implement the calling and demonstration of the overall process

---
+++ src/assessment/AssignmentOne.java
@@ -0,0 +1,13 @@
+package assessment;
+
+/**
+ * Class：AssignmentOne
+ */
+public class AssignmentOne {
+    public static void main(String[] args) {
+        // Part3
+        ClassObjectDemo.runPart3();
+
+        AppointmentManager.runPart5();
+    }
+}
